c5d941f687802f47a809cf8dc3430eddecd3b8a1
rrahman <rashidur92@hotmail.com>
2024-12-09 16:44:02 +0000

    got string concating versions of fizzbuzz working with pointers and buffers, but arbitrarily chosen buffer size instead of calculated

--- c-projects/src/lib.c
+++ c-projects/src/lib.c
@@ -1,18 +1,61 @@
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
 int add(int a, int b) {
   return a + b;
 }
 
 char *fizzbuzz_pointer(int maxNum){
+  int maxCharStringLen;
+  int bufLen = 1000;
+  // int bufLen = ((1 + maxNum / 15)*strlen("fizzbuzz, ") + (1 + maxNum / 3)*strlen("fizz, ") + (1 + maxNum / 5)* strlen("buzz, ") + maxNum*10)*sizeof(char);
+  char *buffer = malloc(bufLen);
 
-}
-
-int fizzbuzz_buffer(char buffer, int bufLen, int maxNum){
-  for (int i=0; i<maxNum; i++){
-
+  for (int i=1; i<=maxNum; i++){
+    if (i % 15 == 0){
+      strncat(buffer, "fizzbuzz", bufLen);
+    }else if (i % 3 == 0){
+      strncat(buffer, "fizz", bufLen);
+    } else if (i % 5 == 0){
+      strncat(buffer, "buzz", bufLen);
+    } else {
+      //can only do numbers upto 10^9
+      char str[10];
+      snprintf(str, sizeof(str), "%d", i);
+      strncat(buffer, str, bufLen);
+    }
+    if (i < maxNum){
+      strncat(buffer, ", ", bufLen);
+    }
   }
+  strncat(buffer, ".", bufLen);
+  return buffer;
 }
 
-int fizzbuzz_print(int maxNum){
+void fizzbuzz_buffer(char *buffer, size_t bufLen, int maxNum){
+  
+  for (int i=1; i<=maxNum; i++){
+    if (i % 15 == 0){
+      strncat(buffer, "fizzbuzz", bufLen);
+    }else if (i % 3 == 0){
+      strncat(buffer, "fizz", bufLen);
+    } else if (i % 5 == 0){
+      strncat(buffer, "buzz", bufLen);
+    } else {
+      //can only do numbers upto 10^9
+      char str[10];
+      snprintf(str, sizeof(str), "%d", i);
+      strncat(buffer, str, bufLen);
+    }
+    if (i < maxNum){
+      strncat(buffer, ", ", bufLen);
+    }
+  }
+  strncat(buffer, ".", bufLen);
+}
+
+void fizzbuzz_print(int maxNum){
   for (int i = 1; i <= maxNum; i++){
     if (i % 15 == 0){
       puts("fizzbuzz");
--- c-projects/src/main.c
+++ c-projects/src/main.c
@@ -4,5 +4,13 @@
 int main() {
   puts("attempting printing fizzbuzz");
   fizzbuzz_print(30);
+  puts("attempting buffer version of fizzbuzz");
+  char result[1000] = {0};
+  fizzbuzz_buffer(result, sizeof(result), 50);
+  printf("%s\n", result);
+  puts("attempting pointer version of fizzbuzz");
+  char *buffer = fizzbuzz_pointer(50);
+  printf("%s\n", buffer);
+  free(buffer);
   return 0;
 }
